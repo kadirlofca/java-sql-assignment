d468defcc9993ae8f39de43d457c28a225a595f3
Kadir Lofca <kadirlofca@outlook.com>
2025-03-25 18:00:13 -0400

    init

---
+++ src/main/java/me/kadirlofca/java_sql_assignment/JavaSqlAssignmentApplication.java
@@ -0,0 +1,13 @@
+package me.kadirlofca.java_sql_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSqlAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSqlAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kadirlofca/java_sql_assignment/JavaSqlAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kadirlofca.java_sql_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSqlAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
