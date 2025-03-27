0d8bcd2a181ef003796ca455509391dfe0e99c69
Kadir Lofca <kadirlofca@outlook.com>
2025-03-27 16:32:08 -0400

    Added class for employee row

---
+++ src/main/java/me/kadirlofca/java_sql_assignment/Employee.java
@@ -0,0 +1,64 @@
+package me.kadirlofca.java_sql_assignment;
+import com.opencsv.bean.CsvBindByName;
+
+public class Employee {
+    @CsvBindByName
+    private String Employee_ID;
+
+    @CsvBindByName
+    private String Full_Name;
+    
+    @CsvBindByName
+    private String Email;
+
+    @CsvBindByName
+    private String Phone_Number;
+
+    @CsvBindByName
+    private String Date_of_Joining;
+
+    @CsvBindByName
+    private String Salary;
+
+    @CsvBindByName
+    private String Department;
+
+    @CsvBindByName
+    private String Status;
+
+    @CsvBindByName
+    private String Manager_ID;
+
+    @CsvBindByName
+    private String Job_Title;
+
+    @CsvBindByName
+    private String Gender;
+
+    @CsvBindByName
+    private String Marital_Status;
+
+    @CsvBindByName
+    private String DOB;
+
+    @CsvBindByName
+    private String Address; 
+
+    @CsvBindByName
+    private String City;
+
+    @CsvBindByName
+    private String State;
+
+    @CsvBindByName
+    private String Country;
+
+    @CsvBindByName
+    private String Zip_Code;
+
+    @CsvBindByName
+    private String Emergency_Contact;
+
+    @CsvBindByName
+    private String Emergency_Contact_Name;
+}
--- src/main/java/me/kadirlofca/java_sql_assignment/JavaSqlAssignmentApplication.java
+++ src/main/java/me/kadirlofca/java_sql_assignment/JavaSqlAssignmentApplication.java
@@ -2,12 +2,36 @@ package me.kadirlofca.java_sql_assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.FileReader;
+
+import com.opencsv.CSVIterator;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
 
 @SpringBootApplication
 public class JavaSqlAssignmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSqlAssignmentApplication.class, args);
-	}
 
+		CSVReader reader = new CSVReaderBuilder(new FileReader("dataset.csv")).build();
+		CSVIterator iterator = new CSVIterator(reader);
+		for(String[] nextLine : iterator) {
+			// nextLine[] is an array of values from the line
+			System.out.println(nextLine[0] + nextLine[1] + "etc...");
+     	}
+
+	// try {
+	// 	CSVReader reader = new CSVReaderBuilder(new FileReader("dataset.csv")).build();
+	// 	String [] nextLine;
+	// 	while ((nextLine = reader.readNext()) != null) {
+	// 		for(int i = 0; i < )
+	// 	   System.out.println(nextLine[0] + nextLine[1] + "etc...");
+	// 	}
+	// }
+	// catch (Exception e) {
+	// 	System.err.println(e);
+	// }
+	}
 }
