bee1582d40f27bff25bb4bd5faef1dc357154086
Kadir Lofca <kadirlofca@outlook.com>
2025-04-03 00:48:18 -0400

    removed mysql from shell.nix. - services.mysql should be enabled in system config

--- shell.nix
+++ shell.nix
@@ -1,10 +1,20 @@
-let
-  pkgs = import <nixpkgs> { };
-in
+{ pkgs ? import <nixpkgs> {config.allowUnfree = true;} }:
+
 pkgs.mkShell {
+  buildInputs = [
+    (pkgs.vscode-with-extensions.override {
+      vscodeExtensions = with pkgs.vscode-extensions; [
+        visualstudioexptteam.vscodeintellicode
+        vscjava.vscode-java-pack
+        vscjava.vscode-maven
+        vscjava.vscode-java-debug
+        redhat.java
+      ];
+    })
+  ];
+
   nativeBuildInputs = with pkgs; [
     jdk
     maven
-    mysql84
   ];
 }
